87fcd82723f4f0e73218df9db92c4ae13e3e63fd
陈远山 <chenysh521@126.com>
2015-06-25 11:05:19 +0800

    finish

--- CYSAnimationTest/ViewController4.m
+++ CYSAnimationTest/ViewController4.m
@@ -17,22 +17,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.myPlaneView.frame = CGRectMake(10, 300, self.myPlaneView.bounds.size.width, self.myPlaneView.bounds.size.height);
     [self planeDepart];
     // Do any additional setup after loading the view.
 }
 
 - (void)planeDepart{
     CGPoint originalCenter = self.myPlaneView.center;
-    [UIView animateKeyframesWithDuration:1.5 delay:0.0 options:UIViewKeyframeAnimationOptionRepeat animations:^{
+    //需要注意的一点是addKeyframeWithRelativeStartTime中的startTime和relativeDuration都是相对与整个关键帧动画的持续时间（这里是4秒）的百分比，设置成0.5就代表4*0.5＝2（秒
+    [UIView animateKeyframesWithDuration:4 delay:0.0 options:UIViewKeyframeAnimationOptionRepeat animations:^{
         [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:0.25 animations:^{
-            CGPoint center = self.myPlaneView.center;
+            CGPoint center = originalCenter;
             center.x += 80.0;
-            center.y += 10.0;
+            center.y -= 10.0;
             self.myPlaneView.center = center;
         }];
-        [UIView addKeyframeWithRelativeStartTime:0.1 relativeDuration:0.4 animations:^{
+        [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:0.4 animations:^{
             self.myPlaneView.transform = CGAffineTransformMakeRotation(-M_PI_4/2);
         }];
+        
+        [UIView addKeyframeWithRelativeStartTime:0.25 relativeDuration:0.25 animations:^{
+            CGPoint center = originalCenter;
+            center.x += 160.0;
+            center.y -= 50.0;
+            self.myPlaneView.center = center;
+            self.myPlaneView.alpha = 0.0;
+        }];
+        
         [UIView addKeyframeWithRelativeStartTime:0.51 relativeDuration:0.01 animations:^{
             self.myPlaneView.transform = CGAffineTransformIdentity;
             self.myPlaneView.center = CGPointMake(0.0, originalCenter.y);
